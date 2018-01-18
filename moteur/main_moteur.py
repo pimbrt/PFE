93a37bd1e36761bb5636a0cbc22da36937a551f3
Pierre Imbert <pierrembrt@gmail.com>
2018-01-18 15:45:28 +0100

    timer

--- moteur/main_moteur.py
+++ moteur/main_moteur.py
@@ -6,6 +6,7 @@ Created on Thu Jan 18 14:08:11 2018
 @author: pierre
 """
 import motor.py as motor
+import time
 
 moteur = motor.Moteur()
 
@@ -16,25 +17,31 @@ angle_have=0
 angle = angle_needed-angle_have
 
 if angle > 0:
+    start = time.clock
     while angle_have > angle_needed:
         
         moteur.moteur1(1)
         
         angle_have = find_ovale()
-        
+    
+    time_loop = time.clock-start
     moteur.stop_Moteur()
-    moteur.reverse_Moteur1(1)#trouver un moyen d'avoir le temps ou la longeur
+    moteur.reverse_Moteur1(1)
+    time.sleep(time_loop)
     moteur.stop_Moteur()
     
 elif angle < 0:
+    start = time.clock
     while angle_needed > angle_have:
         
         moteur.moteur2(1)
         
         angle_have = find_ovale()
-        
+    
+    time_loop = time.clock-start
     moteur.stop_Moteur()
-    moteur.reverse_Moteur2(1)#trouver un moyen d'avoir le temps ou la longeur
+    moteur.reverse_Moteur2(1)
+    time.sleep(time_loop)
     moteur.stop_Moteur()
     
 else:
