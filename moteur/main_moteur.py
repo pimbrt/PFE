56f5a7f06e797f63f8bd0e81b5eb4d04d7dd858a
Pierre Imbert <pierrembrt@gmail.com>
2018-01-22 13:59:04 +0100

    def moteur

--- moteur/main_moteur.py
+++ moteur/main_moteur.py
@@ -10,39 +10,37 @@ import time
 
 moteur = motor.Moteur()
 
-#recupérer les angles
-angle_needed=0
-angle_have=0
+def moveMotors(angle_needed,angle_have):
 
-angle = angle_needed-angle_have
+	angle = angle_needed-angle_have
 
-if angle > 0:
-    start = time.clock
-    while angle_have > angle_needed:
-        
-        moteur.moteur1(1)
-        
-        angle_have = find_ovale()
-    
-    time_loop = time.clock-start
-    moteur.stop_Moteur()
-    moteur.reverse_Moteur1(1)
-    time.sleep(time_loop)
-    moteur.stop_Moteur()
-    
-elif angle < 0:
-    start = time.clock
-    while angle_needed > angle_have:
-        
-        moteur.moteur2(1)
-        
-        angle_have = find_ovale()
-    
-    time_loop = time.clock-start
-    moteur.stop_Moteur()
-    moteur.reverse_Moteur2(1)
-    time.sleep(time_loop)
-    moteur.stop_Moteur()
-    
-else:
-    moteur.stop_Moteur()
+	if angle > 0:
+	    start = time.clock
+	    while angle_have > angle_needed:
+	        
+	        moteur.moteur1(1)
+	        
+	        angle_have = find_ovale()
+	    
+	    time_loop = time.clock-start
+	    moteur.stop_Moteur()
+	    moteur.reverse_Moteur1(1)
+	    time.sleep(time_loop)
+	    moteur.stop_Moteur()
+	    
+	elif angle < 0:
+	    start = time.clock
+	    while angle_needed > angle_have:
+	        
+	        moteur.moteur2(1)
+	        
+	        angle_have = find_ovale()
+	    
+	    time_loop = time.clock-start
+	    moteur.stop_Moteur()
+	    moteur.reverse_Moteur2(1)
+	    time.sleep(time_loop)
+	    moteur.stop_Moteur()
+	    
+	else:
+	    moteur.stop_Moteur()
